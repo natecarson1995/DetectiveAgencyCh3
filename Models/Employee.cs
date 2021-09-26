2fdeeca9af39ae1e02032166b449446bba6c2029
carson29 <carson29@mail.nmc.edu>
2021-09-26 13:32:56 -0400

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DetectiveAgency.Models
+{
+    public class Employee
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(10,75,ErrorMessage ="Hours must be between 10 and 75.")]
+        public double Hours { get; set; }
+
+        [Required]
+        [Range(15,75, ErrorMessage ="Rate should be between $15 and $75.")]
+        public double Rate { get; set; }
+
+        public double CalcSalary()
+        {
+            double grossPay, regPay;
+            double overtime = 0;
+
+            if (Hours>40) {
+                regPay = 40 * Rate;
+                overtime = (Hours - 40) * (1.5 * Rate);
+            } else {
+                regPay = Hours * Rate;
+            }
+            grossPay = regPay + overtime;
+
+            return grossPay;
+        }
+    }
+}
